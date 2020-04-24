4c67b4acfc44b374ef091b84f430c640e2877f81
kosskeetit <leroykipruto57@gmail.com>
2020-04-25 00:40:07 +0300

    first puh to github

---
+++ app/src/main/java/com/example/navigationdrawer/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,29 @@
+package com.example.navigationdrawer.ui.gallery
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.navigationdrawer.R
+
+class GalleryFragment : Fragment() {
+
+
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+
+        val root = inflater.inflate(R.layout.fragment_gallery, container, false)
+        val textView: TextView = root.findViewById(R.id.text_gallery)
+        textView.text = "Gallery Fragment"
+
+        return root
+    }
+}
